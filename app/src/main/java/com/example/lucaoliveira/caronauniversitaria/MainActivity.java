c7f1fce195f76d3d650a0e96c21e6701cb465b89
Lucas Calegari de Oliveira <Lucas Calegari de Oliveira>
2016-06-27 22:32:57 -0300

    Main Activity finalizando funcionalidade dos botoes

--- app/src/main/java/com/example/lucaoliveira/caronauniversitaria/MainActivity.java
+++ app/src/main/java/com/example/lucaoliveira/caronauniversitaria/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.lucaoliveira.caronauniversitaria;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.lucaoliveira.caronauniversitaria.data.User;
 import com.example.lucaoliveira.caronauniversitaria.webservices.WebServiceTask;
+import com.example.lucaoliveira.caronauniversitaria.webservices.WebServicesUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private UserInfoTask mUserInfoTask = null;
@@ -56,6 +65,78 @@ public class MainActivity extends AppCompatActivity {
         mNoteText.setText(user.getNote() == null ? "" : user.getNote());
     }
 
+    public void clickUpdateButton(View view) {
+        if (mPasswordText.getText().toString().trim().length() >= 5) {
+            showProgress(true);
+            mUserEditTask = new UserEditTask();
+            mUserEditTask.execute();
+        } else {
+            Toast.makeText(this, R.string.error_passoword_length, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void clickDeleteButton(View view) {
+        showConfirmationDialog(new ConfirmationListener() {
+
+            @Override
+            public void onConfirmation(boolean isConfirmed) {
+                if (isConfirmed) {
+                    showProgress(true);
+                    mUserDeleteTask = new UserDeleteTask();
+                    mUserDeleteTask.execute();
+                }
+
+            }
+        });
+    }
+
+    public void clickResetButton(View view) {
+        showConfirmationDialog(new ConfirmationListener() {
+            @Override
+            public void onConfirmation(boolean isConfirmed) {
+                if (isConfirmed) {
+                    showProgress(true);
+                    mUserResetTask = new UserResetTask();
+                    mUserResetTask.execute();
+                }
+            }
+        });
+    }
+
+    public void clickSignOutButton(View view) {
+        showLoginScreen();
+    }
+
+    private void showLoginScreen() {
+        Intent intent = new Intent(MainActivity.this, LoginRegisterActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void showConfirmationDialog(final ConfirmationListener confirmationListener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmação");
+        builder.setMessage("Tem certeza? Essa operação não pode ser desfeita!");
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                confirmationListener.onConfirmation(true);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                confirmationListener.onConfirmation(false);
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(true);
+        alertDialog.show();
+    }
+
     private abstract class ActivityWebServiceTask extends WebServiceTask {
         public ActivityWebServiceTask(WebServiceTask webServiceTask) {
             super(MainActivity.this);
@@ -83,7 +164,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public boolean performRequest() {
-            return true;
+            ContentValues contentValues = new ContentValues();
+            User user = RESTServiceApplication.getInstance().getUser();
+            contentValues.put(Constants.ID, user.getId());
+            contentValues.put(Constants.ACCESS_TOKEN, RESTServiceApplication.getInstance().getAccessToken());
+
+            JSONObject object = WebServicesUtils.requestJSONObject(Constants.INFO_URL, WebServicesUtils.METHOD.GET, contentValues, null);
+            if (!hasError(object)) {
+                JSONArray jsonArray = object.optJSONArray(Constants.INFO); // informação vem em formato de json
+                JSONObject jsonObject = jsonArray.optJSONObject(0); // pegando o index 0 do array
+
+                user.setName(jsonObject.optString(Constants.NAME));
+                if (user.getName().equalsIgnoreCase("null")) {
+                    user.setName(null);
+                }
+
+                user.setName(jsonObject.optString(Constants.PHONE_NUMBER));
+                if (user.getPhoneNumber().equalsIgnoreCase("null")) {
+                    user.setPhoneNumber(null);
+                }
+
+                user.setNote(jsonObject.optString(Constants.NOTE));
+                if (user.getNote().equalsIgnoreCase("null")) {
+                    user.setNote(null);
+                }
+
+                user.setId(jsonObject.optLong(Constants.ID_INFO));
+                return true;
+            }
+            return false;
         }
     }
 
@@ -93,7 +202,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public boolean performRequest() {
-            return true;
+            ContentValues contentValues = new ContentValues();
+            User user = RESTServiceApplication.getInstance().getUser();
+            contentValues.put(Constants.ID, user.getId());
+            contentValues.put(Constants.NAME, user.getName());
+            contentValues.put(Constants.PASSWORD, user.getPassword());
+            contentValues.put(Constants.PHONE_NUMBER, mPhoneNumberText.getText().toString());
+            contentValues.put(Constants.NOTE, mNoteText.getText().toString());
+
+            ContentValues urlValues = new ContentValues();
+            urlValues.put(Constants.ACCESS_TOKEN, RESTServiceApplication.getInstance().getAccessToken());
+
+            JSONObject obj = WebServicesUtils.requestJSONObject(Constants.UPDATE_URL, WebServicesUtils.METHOD.POST, urlValues, contentValues);
+
+            if (!hasError(obj)) {
+                JSONArray jsonArray = obj.optJSONArray(Constants.INFO);
+                JSONObject jsonObject = jsonArray.optJSONObject(0);
+                user.setName(jsonObject.optString(Constants.NAME));
+                user.setPhoneNumber(jsonObject.optString(Constants.PHONE_NUMBER));
+                user.setNote(jsonObject.optString(Constants.NOTE));
+                user.setPassword(jsonObject.optString(Constants.PASSWORD));
+                return true;
+            }
+            return false;
         }
     }
 
@@ -103,7 +234,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public boolean performRequest() {
-            return true;
+            ContentValues contentValues = new ContentValues();
+            User user = RESTServiceApplication.getInstance().getUser();
+            contentValues.put(Constants.ID, user.getId());
+            contentValues.put(Constants.ACCESS_TOKEN, RESTServiceApplication.getInstance().getAccessToken());
+
+            JSONObject obj = WebServicesUtils.requestJSONObject(Constants.RESET_URL, WebServicesUtils.METHOD.POST, contentValues, null);
+
+            if (!hasError(obj)) {
+                user.setName("");
+                user.setPhoneNumber("");
+                user.setNote("");
+                return true;
+            }
+            return false;
         }
     }
 
@@ -113,7 +257,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public boolean performRequest() {
-            return true;
+            ContentValues contentValues = new ContentValues();
+            User user = RESTServiceApplication.getInstance().getUser();
+            contentValues.put(Constants.ID, user.getId());
+            contentValues.put(Constants.ACCESS_TOKEN, RESTServiceApplication.getInstance().getAccessToken());
+
+            JSONObject obj = WebServicesUtils.requestJSONObject(Constants.DELETE_URL, WebServicesUtils.METHOD.DELETE, contentValues, null);
+
+            if (!hasError(obj)) {
+                RESTServiceApplication.getInstance().setUser(null);
+                return true;
+            }
+
+            return false;
         }
     }
 
