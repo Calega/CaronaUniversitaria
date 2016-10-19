0edd871dabca2e20b43342fbd039143e50481333
Lucas Calegari <lcalegari17@gmail.com>
2016-10-19 01:42:55 -0200

    Preparing notification adapter

---
+++ app/src/main/java/com/example/lucaoliveira/caronauniversitaria/ui/adapter/NotificationAdapter.java
@@ -0,0 +1,8 @@
+package com.example.lucaoliveira.caronauniversitaria.ui.adapter;
+
+/**
+ * Created by lucaoliveira on 19/10/16.
+ */
+
+public class NotificationAdapter {
+}
--- app/src/main/java/com/example/lucaoliveira/caronauniversitaria/ui/fragment/NotificationsFragment.java
+++ app/src/main/java/com/example/lucaoliveira/caronauniversitaria/ui/fragment/NotificationsFragment.java
@@ -27,6 +27,6 @@ public class NotificationsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_list, container, false);
+        return inflater.inflate(R.layout.fragment_notification, container, false);
     }
 }
